0f70d04661fa3d7a5165b296a09605c50698bef2
Padraic-O-Mhuiris <patrick.morris.310@gmail.com>
2025-02-24 22:26:17 +0000

    Oxygen disko workings

--- hosts/Oxygen/disk.nix
+++ hosts/Oxygen/disk.nix
@@ -1,135 +1,109 @@
 _: let
-  ESP = {
-    size = "500M";
-    type = "EF00";
-    content = {
-      type = "filesystem";
-      format = "vfat";
-      mountpoint = "/boot";
-      mountOptions = ["umask=0077"];
+  partition = {
+    boot = {
+      size = "500M";
+      type = "EF00";
+      content = {
+        type = "filesystem";
+        format = "vfat";
+        mountpoint = "/boot";
+        mountOptions = ["umask=0077"];
+      };
+    };
+
+    root."/" = {
+      fsType = "tmpfs";
+      mountOptions = [
+        "size=500M"
+      ];
+    };
+
+    swap = {
+      size = "16G";
+      content = {
+        type = "swap";
+        randomEncryption = true;
+        priority = 100;
+      };
+    };
+
+    luks = {
+      size = "100%";
+      content = {
+        type = "luks";
+        name = "crypted";
+        extraOpenArgs = [];
+        settings = {
+          keyFile = "/tmp/secret.key";
+          allowDiscards = true;
+        };
+        content = {
+          type = "lvm_pv";
+          vg = "pool";
+        };
+      };
     };
   };
 
-  luks = {
-    size = "100%";
+  nvme = {
+    type = "disk";
+    device = "/dev/disk/by-id/nvme-Samsung_SSD_970_EVO_Plus_2TB_S4J4NF0NC04658B";
     content = {
-      type = "luks";
-      name = "crypted";
-      extraOpenArgs = [];
-      settings = {
-        keyFile = "/tmp/secret.key";
-        allowDiscards = true;
+      type = "gpt";
+      partitions = {
+        inherit (partition) boot luks swap;
       };
-      # additionalKeyFiles = [ "/tmp/additionalSecret.key" ];
+    };
+  };
+
+  sda = {
+    type = "disk";
+    device = "/dev/disk/by-id/ata-Samsung_SSD_860_EVO_2TB_S4X1NJ0NB04835M";
+    content = {
+      type = "gpt";
+      partitions = {
+        inherit (partition) luks;
+      };
+    };
+  };
+
+  volumes = {
+    persist = {
+      size = "10G";
       content = {
-        type = "lvm_pv";
-        vg = "pool";
+        type = "filesystem";
+        format = "ext4";
+        mountpoint = "/persist";
+      };
+    };
+
+    home = {
+      size = "500G";
+      content = {
+        type = "filesystem";
+        format = "ext4";
+        mountpoint = "/home";
+      };
+    };
+
+    nix = {
+      size = "100%";
+      content = {
+        type = "filesystem";
+        format = "ext4";
+        mountpoint = "/nix";
       };
     };
   };
 in {
   disko.devices = {
-    disk = {
-      nvme = {
-        type = "disk";
-        device = "/dev/disk/by-id/nvme-Samsung_SSD_970_EVO_Plus_2TB_S4J4NF0NC04658B";
-        content = {
-          type = "gpt";
-          partitions = {
-            inherit ESP luks;
-          };
-        };
-      };
+    disk = {inherit nvme sda;};
 
-      sda = {
-        type = "disk";
-        device = "/dev/disk/by-id/ata-Samsung_SSD_860_EVO_2TB_S4X1NJ0NB04835M";
-        content = {
-          type = "gpt";
-          partitions = {
-            inherit luks;
-          };
-        };
-      };
-    };
+    nodev = partition.root;
 
-    lvm_vg = {
-      pool = {
-        type = "lvm_vg";
-        lvs = {
-          root = {
-            size = "100%";
-            content = {
-              type = "filesystem";
-              format = "ext4";
-              mountpoint = "/";
-              mountOptions = ["defaults"];
-            };
-          };
-          home = {
-            size = "10M";
-            content = {
-              type = "filesystem";
-              format = "ext4";
-              mountpoint = "/home";
-            };
-          };
-          raw = {
-            size = "10M";
-          };
-        };
-      };
+    lvm_vg.pool = {
+      type = "lvm_vg";
+      lvs = {inherit (volumes) persist home nix;};
     };
   };
 }
-# {
-#   nvme = {
-#     type = "disk";
-#     device = "/dev/disk/by-id/nvme-Samsung_SSD_970_EVO_Plus_2TB_S4J4NF0NC04658B";
-#     content = {
-#       type = "gpt";
-#       partitions = {
-#         ESP = {
-#           type = "EF00";
-#           size = "64M";
-#           content = {
-#             type = "filesystem";
-#             format = "vfat";
-#             mountpoint = "/boot";
-#           };
-#         };
-#         swap = {
-#           size = "12G";
-#           content = {
-#             type = "swap";
-#             randomEncryption = true;
-#           };
-#         };
-#         zroot = {
-#           size = "100%";
-#           content = {
-#             type = "zfs";
-#             pool = "rpool";
-#           };
-#         };
-#       };
-#     };
-#   };
-#   sda = {
-#     type = "disk";
-#     device = "/dev/disk/by-id/ata-Samsung_SSD_860_EVO_2TB_S4X1NJ0NB04835M";
-#     content = {
-#       type = "gpt";
-#       partitions = {
-#         zroot = {
-#           size = "100%";
-#           content = {
-#             type = "zfs";
-#             pool = "rpool";
-#           };
-#         };
-#       };
-#     };
-#   };
-# }
-
