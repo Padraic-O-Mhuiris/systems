0b102f66e463cd9c59b0cf7dc3d8acddc44b4e35
Padraic-O-Mhuiris <patrick.morris.310@gmail.com>
2025-02-26 21:13:43 +0000

    improved iso network connection systemd script

--- iso/default.nix
+++ iso/default.nix
@@ -33,7 +33,7 @@
           nix.extraOptions = "experimental-features = nix-command flakes";
 
           networking = {
-            hostName = "NIXOS_ISO_HOST";
+            hostName = "iso";
             networkmanager.enable = true;
             wireless.enable = false;
           };
@@ -50,11 +50,9 @@
 
           environment.systemPackages = with pkgs; [
             vim
+            htop
             git
             rsync
-            (pkgs.writeShellScriptBin "connect-wifi" ''
-              nmcli device wifi connect VM9598311 password cjDpkp9F5sdf
-            '')
           ];
 
           users.extraUsers.root.openssh.authorizedKeys.keys = [
@@ -62,6 +60,36 @@
           ];
 
           systemd.services.sshd.wantedBy = pkgs.lib.mkForce ["multi-user.target"];
+
+          systemd.services.connectToLocalInternet = {
+            description = "Connect to local Wi-Fi on boot using nmcli";
+
+            after = ["default.target"];
+            wantedBy = ["default.target"];
+
+            script = ''
+              #!/usr/bin/env bash
+              SSID="VM2888317"
+              PASSWORD="cedkkdJzbxzgxe2a"
+
+              for i in {1..30}; do
+                if ${pkgs.networkmanager}/bin/nmcli general status &> /dev/null; then
+                  echo "NetworkManager is ready."
+                  break
+                fi
+                echo "Waiting for NetworkManager..."
+                sleep 1
+              done
+
+              echo "Attempting to connect to Wi-Fi: $SSID"
+              ${pkgs.networkmanager}/bin/nmcli device wifi connect "$SSID" password "$PASSWORD"
+            '';
+
+            serviceConfig = {
+              Type = "oneshot";
+              RemainAfterExit = true;
+            };
+          };
 
           system.stateVersion = l.mkDefault "24.11";
 
