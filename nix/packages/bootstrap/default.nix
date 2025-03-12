8a1890d145561ce8f6a9e2f25a626bed8f46c7c0
Padraic-O-Mhuiris <patrick.morris.310@gmail.com>
2025-03-12 23:30:15 +0000

    made the bootstrap script more general

--- nix/packages/bootstrap/default.nix
+++ nix/packages/bootstrap/default.nix
@@ -1,35 +1,30 @@
-{
-  inputs,
-  self,
-  ...
-}: let
-  inherit (self) lib;
-in {
+{inputs, ...}: {
   perSystem = {pkgs, ...}: let
     nixos-anywhere =
       inputs.nixos-anywhere.packages.${pkgs.system}.default;
   in {
-    packages = lib.mkMerge (lib.mapByHostName (host: {
-      "bootstrap-${host}" = pkgs.writeShellScriptBin "bootstrap-${host}" ''
-        temp=$(mktemp -d)
+    packages.bootstrap = pkgs.writeShellScriptBin "bootstrap" ''
+      HOST=$1
+      URL=$2
 
-        cleanup() {
-          rm -rf "$temp"
-        }
-        trap cleanup EXIT
+      temp=$(mktemp -d)
 
-        install -d -m755 "$temp/etc/ssh"
+      cleanup() {
+        rm -rf "$temp"
+      }
+      trap cleanup EXIT
 
-        ${pkgs.pass}/bin/pass show systems/ssh/${host}/ssh_host_ed25519_key.pub > "$temp/etc/ssh/ssh_host_ed25519_key.pub"
-        ${pkgs.pass}/bin/pass show systems/ssh/${host}/ssh_host_ed25519_key > "$temp/etc/ssh/ssh_host_ed25519_key"
+      install -d -m755 "$temp/etc/ssh"
 
-        ${nixos-anywhere}/bin/nixos-anywhere \
-          --extra-files "$temp" \
-          --disk-encryption-keys /tmp/secret.key <(pass show systems/disks/${host}) \
-          --flake '.#${host}' \
-          --phases 'kexec,disko,install,reboot' \
-          root@${inputs.secrets.vars.HOSTS."${host}".LOCAL_IP}
-      '';
-    }));
+      ${pkgs.pass}/bin/pass show "systems/ssh/$HOST/ssh_host_ed25519_key.pub" > "$temp/etc/ssh/ssh_host_ed25519_key.pub"
+      ${pkgs.pass}/bin/pass show "systems/ssh/$HOST/ssh_host_ed25519_key" > "$temp/etc/ssh/ssh_host_ed25519_key"
+
+      ${nixos-anywhere}/bin/nixos-anywhere \
+        --extra-files "$temp" \
+        --disk-encryption-keys /tmp/secret.key <(pass show systems/disks/$HOST) \
+        --flake ".#$HOST" \
+        --phases 'kexec,disko,install,reboot' \
+        $URL
+    '';
   };
 }
