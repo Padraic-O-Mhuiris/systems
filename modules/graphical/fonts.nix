a2fdf3f168d4ed3702922bd4fd200875a4842e3c
Padraic-O-Mhuiris <patrick.morris.310@gmail.com>
2025-05-18 21:59:39 +0100

    remove some fonts

--- modules/graphical/fonts.nix
+++ modules/graphical/fonts.nix
@@ -11,15 +11,6 @@ in {
 
     home.packages = with pkgs; [
       berkeley-mono
-      iosevka
-      corefonts
-      dejavu_fonts
-      liberation_ttf
-      roboto
-      fira-code
-      jetbrains-mono
-      siji
-      font-awesome
       nerdfonts
     ];
   };
