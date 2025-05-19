82543d89019e03cdf22ff98dc06cf4cef6e85155
Padraic-O-Mhuiris <patrick.morris.310@gmail.com>
2025-05-19 21:19:14 +0100

    update gpg

--- modules/security/gpg.nix
+++ modules/security/gpg.nix
@@ -3,6 +3,20 @@
   vars,
   ...
 }: {
+  environment.systemPackages = with pkgs; [
+    yubikey-personalization
+    yubikey-manager
+  ];
+
+  # Allows for smartcard detection
+  services.pcscd.enable = true;
+  services.udev.packages = with pkgs; [
+    yubikey-personalization
+    libu2f-host
+  ];
+
+  hardware.gpgSmartcards.enable = true;
+
   home-manager.users.${vars.PRIMARY_USER.NAME} = {config, ...}: {
     programs.gpg = {
       enable = true;
