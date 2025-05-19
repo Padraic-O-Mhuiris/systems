91a7a73c52bb6b03497fe42f5932650db38bec7e
Padraic-O-Mhuiris <patrick.morris.310@gmail.com>
2025-05-19 21:39:52 +0100

    updated gpg keys

--- modules/security/gpg.nix
+++ modules/security/gpg.nix
@@ -9,11 +9,14 @@
   ];
 
   # Allows for smartcard detection
-  services.pcscd.enable = true;
-  services.udev.packages = with pkgs; [
-    yubikey-personalization
-    libu2f-host
-  ];
+  services = {
+    pcscd.enable = true;
+    udev.packages = with pkgs; [
+      yubikey-personalization
+      libu2f-host
+    ];
+    yubikey-agent.enable = true;
+  };
 
   hardware.gpgSmartcards.enable = true;
 
@@ -22,6 +25,12 @@
       enable = true;
       mutableKeys = false;
       homedir = "${config.xdg.dataHome}/gnupg";
+      publicKeys = [
+        {
+          text = vars.PRIMARY_USER.GPG_PUBLIC_KEY;
+          trust = 5;
+        }
+      ];
     };
 
     services.gpg-agent = {
