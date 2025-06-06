b4d119201082d692b390a6d584aa46b65a5a74b6
Padraic-O-Mhuiris <patrick.morris.310@gmail.com>
2025-06-06 11:34:15 +0100

    bump secrets

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Description for the project";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
 
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
