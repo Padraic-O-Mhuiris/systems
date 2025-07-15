a08af790ca9ce4b9e2b258c224a53a72809e4110
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2025-07-15 15:37:41 +0000

    more general cleanup

--- hosts/Oxygen/default.nix
+++ hosts/Oxygen/default.nix
@@ -14,6 +14,7 @@
     ./disk.nix
 
     ../../modules/common/home-manager.nix
+    ../../modules/common/nix
 
     ../../modules/networking/wifi.nix
     ../../modules/networking/ssh.nix
@@ -98,50 +99,6 @@
     autologinOnce = true;
   };
 
-  nixpkgs = {
-    config.allowUnfree = lib.mkDefault true;
-  };
-
-  nix = {
-    # pin the registry to avoid downloading and evaling a new nixpkgs version every time
-    registry = lib.mapAttrs (_: v: {flake = v;}) inputs;
-
-    package = pkgs.nixVersions.nix_2_29;
-
-    # set the path for channels compat
-    nixPath = lib.mapAttrsToList (key: _: "${key}=flake:${key}") config.nix.registry;
-
-    settings = {
-      auto-optimise-store = true;
-      builders-use-substitutes = true;
-      experimental-features = [
-        "nix-command"
-        "flakes"
-      ];
-      flake-registry = "/etc/nix/registry.json";
-
-      # for direnv GC roots
-      keep-derivations = true;
-      keep-outputs = true;
-
-      trusted-users = [
-        "root"
-        "@wheel"
-      ];
-      substituters = ["https://cache.nixos.org?priority=10"];
-      trusted-substituters = [
-        "https://cache.nixos.org/"
-        "https://nix-community.cachix.org"
-        "https://numtide.cachix.org"
-      ];
-      trusted-public-keys = [
-        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
-      ];
-    };
-  };
-
   security.pam.services.sddm.enableGnomeKeyring = true;
 
   services.displayManager = {
@@ -157,39 +114,10 @@
   };
 
   home-manager = {
-    useGlobalPkgs = true;
-    useUserPackages = false;
-    backupFileExtension = "backup";
-    verbose = true;
-
-    users.${vars.PRIMARY_USER.NAME} = {config, ...}: {
+    users.${vars.PRIMARY_USER.NAME} = _: {
       imports = [
         inputs.secrets.homeModules.default
       ];
-
-      home = {
-        homeDirectory = "/home/${vars.PRIMARY_USER.NAME}";
-        preferXdgDirectories = true;
-      };
-
-      xdg = {
-        enable = true;
-        userDirs = {
-          enable = true;
-          createDirectories = true;
-          download = "${config.home.homeDirectory}/downloads";
-          extraConfig = {
-            XDG_CODE_DIR = "${config.home.homeDirectory}/code";
-          };
-          desktop = null;
-          documents = "documents";
-          pictures = null;
-          publicShare = null;
-          templates = null;
-          videos = null;
-          music = null;
-        };
-      };
 
       wayland.windowManager.hyprland.settings = {
         monitor = [
--- modules/users/primary.nix
+++ modules/users/primary.nix
@@ -29,6 +29,28 @@
   };
 
   home-manager.users.${vars.PRIMARY_USER.NAME} = _: {
-    home.homeDirectory = "/home/${vars.PRIMARY_USER.NAME}";
+    home = {
+      homeDirectory = "/home/${vars.PRIMARY_USER.NAME}";
+      preferXdgDirectories = true;
+    };
+
+    xdg = {
+      enable = true;
+      userDirs = {
+        enable = true;
+        createDirectories = true;
+        download = "${config.home.homeDirectory}/downloads";
+        extraConfig = {
+          XDG_CODE_DIR = "${config.home.homeDirectory}/code";
+        };
+        desktop = null;
+        documents = "documents";
+        pictures = null;
+        publicShare = null;
+        templates = null;
+        videos = null;
+        music = null;
+      };
+    };
   };
 }
