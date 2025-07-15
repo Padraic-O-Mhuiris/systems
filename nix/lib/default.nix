e13b4c042b809aeefa614f139c8e1c77b1f1ca70
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2025-07-15 16:09:36 +0000

    cleanup lib

--- nix/lib/default.nix
+++ nix/lib/default.nix
@@ -1,14 +1,3 @@
-{
-  inputs,
-  self,
-  ...
-}: {
-  flake.lib = inputs.nixpkgs.lib.extend (_: lib: let
-    inherit (lib) map attrsToList;
-  in {
-    mapByHostName = fn: (map fn (map ({name, ...}: name) (attrsToList self.nixosConfigurations)));
-
-    # facter specific logic
-    facter = import ./facter.nix {inherit lib;};
-  });
+{inputs, ...}: {
+  flake.lib = inputs.nixpkgs.lib.extend (_: lib: {});
 }
