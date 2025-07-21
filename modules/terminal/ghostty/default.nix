c0529474830e85cdbeed567b3e79227305ff9682
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2025-07-21 18:48:52 +0000

    ghostty

--- modules/terminal/ghostty/default.nix
+++ modules/terminal/ghostty/default.nix
@@ -2,14 +2,14 @@
   home-manager.users.${vars.PRIMARY_USER.NAME} = {
     config,
     pkgs,
-    lib,
+    # lib,
     ...
   }: {
     home.packages = with pkgs; [ghostty];
 
-    home.sessionVariables = {
-      TERMINAL = lib.getExe pkgs.ghostty;
-    };
+    # home.sessionVariables = {
+    #   TERMINAL = lib.getExe pkgs.ghostty;
+    # };
 
     xdg.configFile."ghostty/config".source = config.lib.file.mkOutOfStoreSymlink "/home/${vars.PRIMARY_USER.NAME}/systems/modules/terminal/ghostty/config";
   };
