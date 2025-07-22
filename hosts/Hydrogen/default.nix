c6995470fb666cf55a05253261f941adbee39473
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2025-07-22 12:36:57 +0000

    extact common modules

--- hosts/Hydrogen/default.nix
+++ hosts/Hydrogen/default.nix
@@ -1,52 +1,7 @@
 {vars, ...}: {
   imports = [
     ./disk.nix
-
-    ../../modules/common/home-manager.nix
-    ../../modules/common/nix
-    ../../modules/common/facter.nix
-    ../../modules/common/boot.nix
-    ../../modules/common/login.nix
-    ../../modules/common/secrets.nix
-    ../../modules/common/pkgs.nix
-    ../../modules/common/impermanence.nix
-
-    ../../modules/networking/wifi.nix
-    ../../modules/networking/ssh.nix
-    ../../modules/networking/firewall.nix
-    ../../modules/networking/dns.nix
-    ../../modules/networking/wait-online.nix
-    ../../modules/networking/tailscale.nix
-
-    ../../modules/security/sudo.nix
-    ../../modules/security/gpg.nix
-
-    # ../../modules/apps/firefox.nix
-    ../../modules/apps/zen-browser.nix
-    ../../modules/apps/spotify.nix
-    ../../modules/apps/obsidian.nix
-    ../../modules/apps/steam.nix
-    ../../modules/apps/slack.nix
-
-    # ../../modules/graphical/wm/hyprland.nix
-    ../../modules/graphical/wm/niri
-    ../../modules/graphical/displayManager.nix
-    ../../modules/graphical/nvidia.nix
-    ../../modules/graphical/fonts.nix
-
-    ../../modules/editors/git.nix
-    ../../modules/editors/helix
-
-    ../../modules/terminal/wezterm
-    ../../modules/terminal/shell/zsh
-    ../../modules/terminal/shell/aliases.nix
     ../../modules/terminal/shell/atuin.nix
-
-    ../../modules/peripherals/audio.nix
-    ../../modules/peripherals/bluetooth.nix
-    ../../modules/peripherals/keyboard.nix
-
-    ../../modules/users/primary.nix
   ];
 
   networking.networkmanager.ensureProfiles.profiles."home".ipv4.address = "192.168.0.51/24";
--- hosts/default.nix
+++ hosts/default.nix
@@ -13,7 +13,52 @@
     inherit root;
   };
 
-  common = [];
+  common = [
+    ../modules/common/home-manager.nix
+    ../modules/common/nix
+    ../modules/common/facter.nix
+    ../modules/common/boot.nix
+    ../modules/common/login.nix
+    ../modules/common/secrets.nix
+    ../modules/common/pkgs.nix
+    ../modules/common/impermanence.nix
+
+    ../modules/networking/wifi.nix
+    ../modules/networking/ssh.nix
+    ../modules/networking/firewall.nix
+    ../modules/networking/dns.nix
+    ../modules/networking/wait-online.nix
+    ../modules/networking/tailscale.nix
+
+    ../modules/security/sudo.nix
+    ../modules/security/gpg.nix
+
+    # ../modules/apps/firefox.nix
+    ../modules/apps/zen-browser.nix
+    ../modules/apps/spotify.nix
+    ../modules/apps/obsidian.nix
+    ../modules/apps/steam.nix
+    ../modules/apps/slack.nix
+
+    ../modules/graphical/wm/niri
+    ../modules/graphical/displayManager.nix
+    ../modules/graphical/nvidia.nix
+    ../modules/graphical/fonts.nix
+
+    ../modules/editors/git.nix
+    ../modules/editors/helix
+
+    ../modules/terminal/wezterm
+    ../modules/terminal/ghostty
+    ../modules/terminal/shell/zsh
+    ../modules/terminal/shell/aliases.nix
+
+    ../modules/peripherals/audio.nix
+    ../modules/peripherals/bluetooth.nix
+    ../modules/peripherals/keyboard.nix
+
+    ../modules/users/primary.nix
+  ];
 in {
   flake.nixosConfigurations = {
     Oxygen = lib.nixosSystem {
