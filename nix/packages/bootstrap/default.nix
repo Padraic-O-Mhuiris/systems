c69603465cdaada22b54b92bb8fe699bd535e3bb
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2025-07-22 15:27:57 +0000

    bootstrap script

--- nix/packages/bootstrap/default.nix
+++ nix/packages/bootstrap/default.nix
@@ -2,6 +2,7 @@
   perSystem = {pkgs, ...}: let
     nixos-anywhere =
       inputs.nixos-anywhere.packages.${pkgs.system}.default;
+    vars = inputs.secrets.vars;
   in {
     packages.bootstrap = pkgs.writeShellScriptBin "bootstrap" ''
       set -euo pipefail
@@ -39,22 +40,36 @@
 
       [[ -z "''${HOST:-}" || -z "''${URL:-}" ]] && usage
 
-      temp=$(mktemp -d)
 
+      temp=$(mktemp -d)
       cleanup() {
         rm -rf "$temp"
       }
       trap cleanup EXIT
 
-      install -d -m755 "$temp/persist/etc/ssh"
 
       # We copy the ssh host key to the persist directory such that it will symlink to the /etc/ssh/ssh_host* files
       # It's also required that sops references /persist/etc/ssh/ in its `age.sshKeyPaths` configuration
+      install -d -m755 "$temp/persist/etc/ssh"
+
       ${pkgs.pass}/bin/pass show "systems/ssh/$HOST/ssh_host_ed25519_key.pub" > "$temp/persist/etc/ssh/ssh_host_ed25519_key.pub"
       ${pkgs.pass}/bin/pass show "systems/ssh/$HOST/ssh_host_ed25519_key" > "$temp/persist/etc/ssh/ssh_host_ed25519_key"
 
       chmod 0600 "$temp/persist/etc/ssh/ssh_host_ed25519_key"
       chmod 0644 "$temp/persist/etc/ssh/ssh_host_ed25519_key.pub"
+
+
+      install -d -m755 "$temp/home/${vars.PRIMARY_USER.NAME}/.ssh"
+
+      chown ${vars.PRIMARY_USER.NAME}:users "$temp/home/${vars.PRIMARY_USER.NAME}"
+      chown ${vars.PRIMARY_USER.NAME}:users "$temp/home/${vars.PRIMARY_USER.NAME}/.ssh"
+
+      ${pkgs.pass}/bin/pass show "systems/users/${vars.PRIMARY_USER.NAME}/ssh/id_ed25519" > "$temp/home/${vars.PRIMARY_USER.NAME}/.ssh/id_ed25519"
+      ${pkgs.pass}/bin/pass show "systems/users/${vars.PRIMARY_USER.NAME}/ssh/id_ed25519.pub" > "$temp/home/${vars.PRIMARY_USER.NAME}/.ssh/id_ed25519.pub"
+
+      chmod 0600 "$temp/home/${vars.PRIMARY_USER.NAME}/.ssh/id_ed25519"
+      chmod 0644 "$temp/home/${vars.PRIMARY_USER.NAME}/.ssh/id_ed25519.pub"
+
 
       ${nixos-anywhere}/bin/nixos-anywhere \
         --extra-files "$temp" \
