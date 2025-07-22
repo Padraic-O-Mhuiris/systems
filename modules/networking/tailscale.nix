7c8bd4e25a3e298b5790a2a3d78ffaeb70814e33
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2025-07-22 18:44:41 +0000

    tailscale

--- modules/networking/tailscale.nix
+++ modules/networking/tailscale.nix
@@ -1,17 +1,9 @@
-{
-  config,
-  pkgs,
-  ...
-}: {
+{pkgs, ...}: {
   environment.systemPackages = [pkgs.tailscale];
-
-  sops.secrets.tailscale_auth = {};
 
   services.tailscale = {
     enable = true;
-    permitCertUid = "patrick.morris.310@gmail.com";
     openFirewall = true;
-    authKeyFile = config.sops.secrets.tailscale_auth.path;
   };
 
   networking.search = ["tail684cf.ts.net"];
