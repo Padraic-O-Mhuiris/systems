f5f163fba680244c1f7a221e7f67d06eb26cd23b
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2025-07-24 15:35:01 +0100

    display-manager auto-login

--- modules/graphical/displayManager.nix
+++ modules/graphical/displayManager.nix
@@ -1,7 +1,9 @@
 {vars, ...}: {
-  services.displayManager.sddm = {
-    enable = true;
-    wayland.enable = true;
+  services.displayManager = {
+    sddm = {
+      enable = true;
+      wayland.enable = true;
+    };
     autoLogin = {
       enable = true;
       user = vars.PRIMARY_USER.NAME;
