b166b9a41da6d4c8f85f6d16729e9dcc9f4d21d1
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2025-07-24 16:30:58 +0100

    aded comment

--- hosts/Oxygen/default.nix
+++ hosts/Oxygen/default.nix
@@ -1,12 +1,17 @@
-{vars, ...}: {
+{
+  pkgs,
+  vars,
+  ...
+}: {
   imports = [
     ./disk.nix
   ];
 
   networking.networkmanager.ensureProfiles.profiles."home".ipv4.address = "192.168.0.50/24";
 
-  # Something related to keyring
+  # gnome-keyring can spawn popups on program launch like spotify.
   environment.variables.XDG_RUNTIME_DIR = "/run/user/${toString vars.PRIMARY_USER.UID}";
+  environment.systemPackages = [pkgs.libsecret];
 
   home-manager.users.${vars.PRIMARY_USER.NAME} = _: {
     programs.niri.settings = {
