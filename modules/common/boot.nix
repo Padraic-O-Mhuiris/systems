c83a9a2fb6c44d8375992a14863398db30aa261d
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2025-07-24 15:33:22 +0100

    force display of boot menu

--- modules/common/boot.nix
+++ modules/common/boot.nix
@@ -1,10 +1,11 @@
 _: {
   boot.loader = {
+    # "null" forces display of boot menu
+    timeout = null;
     systemd-boot = {
       enable = true;
       # When set to true, allows access to kernel cli enabling root access
       editor = false;
-      timeout = 5;
       configurationLimit = 100;
     };
     efi.canTouchEfiVariables = true;
