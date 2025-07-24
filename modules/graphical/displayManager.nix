e2527b329e4a9b9ec90bc12ed99499fe8c1703ac
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2025-07-24 15:28:02 +0100

    consolidate login

--- modules/graphical/displayManager.nix
+++ modules/graphical/displayManager.nix
@@ -1,6 +1,10 @@
-_: {
+{vars, ...}: {
   services.displayManager.sddm = {
     enable = true;
     wayland.enable = true;
+    autoLogin = {
+      enable = true;
+      user = vars.PRIMARY_USER.NAME;
+    };
   };
 }
