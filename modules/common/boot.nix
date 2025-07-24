ea51ab6facda483324f4170cc811365532338aff
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2025-07-24 17:01:34 +0100

    small changes

--- modules/common/boot.nix
+++ modules/common/boot.nix
@@ -1,7 +1,5 @@
 _: {
   boot.loader = {
-    # "null" forces display of boot menu
-    timeout = null;
     systemd-boot = {
       enable = true;
       # When set to true, allows access to kernel cli enabling root access
