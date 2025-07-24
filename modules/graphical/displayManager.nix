48e2ca9d2c1a95787154640c34d3b4783843f23e
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2025-07-24 15:46:47 +0100

    more display management

--- modules/graphical/displayManager.nix
+++ modules/graphical/displayManager.nix
@@ -9,4 +9,15 @@
       user = vars.PRIMARY_USER.NAME;
     };
   };
+
+  # if no display we still want to login to tty
+  services.getty = {
+    autologinUser = vars.PRIMARY_USER.NAME;
+    autologinOnce = true;
+  };
+
+  services.gnome.gnome-keyring.enable = true;
+  security.pam.services.greetd.enableGnomeKeyring = true;
+  security.pam.services.sddm.enableGnomeKeyring = true;
+  environment.variables.XDG_RUNTIME_DIR = "/run/user/$UID";
 }
