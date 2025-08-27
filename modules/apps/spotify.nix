f8bfdb1b534fea50cc1a37332054bdddedc8dc37
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2025-08-27 14:48:13 +0100

    update spotify

--- modules/apps/spotify.nix
+++ modules/apps/spotify.nix
@@ -2,4 +2,9 @@
   home-manager.users.${vars.PRIMARY_USER.NAME} = {pkgs, ...}: {
     home.packages = [pkgs.spotify];
   };
+
+  networking.firewall = {
+    allowedTCPPorts = [57621];
+    allowedUDPPorts = [5353];
+  };
 }
