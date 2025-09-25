83826fc3e7d29fa3a6bf366a8e5ea29a50124174
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2025-09-25 21:42:33 +0100

    update docker to be rootless

--- modules/virtualisation/default.nix
+++ modules/virtualisation/default.nix
@@ -1,4 +1,17 @@
 {vars, ...}: {
-  virtualisation.docker.enable = true;
-  users.users.${vars.PRIMARY_USER.NAME}.extraGroups = ["docker"];
+  virtualisation.docker = {
+    enable = false;
+    rootless = {
+      enable = true;
+      setSocketVariable = true;
+      # Optionally customize rootless Docker daemon settings
+      daemon.settings = {
+        dns = ["1.1.1.1" "8.8.8.8"];
+        registry-mirrors = ["https://mirror.gcr.io"];
+        data-root = "/${vars.PRIMARY_USER.NAME}/.local/state/docker-data";
+      };
+    };
+  };
+
+  # users.users.${vars.PRIMARY_USER.NAME}.extraGroups = ["docker"];
 }
