6e88b6e3b885a857668606af9b0535452ea513be
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2025-09-29 18:19:37 +0100

    added smoother claude-code

--- modules/ai/default.nix
+++ modules/ai/default.nix
@@ -9,34 +9,45 @@
     lib,
     ...
   }: let
-    claudeSrcPath = "/home/${vars.PRIMARY_USER.NAME}/systems/modules/ai/claude";
+    claudeDirPath = "/home/${vars.PRIMARY_USER.NAME}/systems/modules/ai/claude";
+
+    claudeCodeSettings = pkgs.writeTextFile {
+      name = "claude-code-cc-settings.json";
+      text =
+        #json
+        ''
+          {
+            "apiKeyHelper": "cat ${config.sops.secrets.anthropic_api_key.path}",
+            "hasCompletedOnboarding": true
+          }
+        '';
+    };
+
+    cc = pkgs.symlinkJoin {
+      name = "claude-code-cc";
+      paths = [inputs.nix-ai-tools.packages.${pkgs.system}.claude-code];
+      buildInputs = [pkgs.makeWrapper];
+      postBuild = ''
+        makeWrapper $out/bin/claude $out/bin/cc \
+          --add-flags "--settings ${claudeCodeSettings}"
+      '';
+    };
   in {
     home.file.".claude" = {
       source = lib.mkForce (
-        config.lib.file.mkOutOfStoreSymlink claudeSrcPath
+        config.lib.file.mkOutOfStoreSymlink claudeDirPath
       );
       recursive = true;
     };
 
-    home.file.".claude/settings.json".text =
-      #json
-      ''
-        {
-          "apiKeyHelper": "cat ${config.sops.secrets.anthropic_api_key.path}"
-        }
-      '';
+    home.packages = with inputs.nix-ai-tools.packages.${pkgs.system}; [
+      claudebox
+      claude-desktop
+      cc
+    ];
 
-    # {
-    #   source = lib.mkForce (
-    #     config.lib.file.mkOutOfStoreSymlink claudeSrcPath
-    #   );
-    #   recursive = true;
-    # };
-
-    home.packages = with inputs.nix-ai-tools.packages.${pkgs.system}; [claudebox claude-desktop claude-code];
-
-    programs.zsh.initContent = ''
-      export ANTHROPIC_API_KEY="$(cat ${config.sops.secrets.anthropic_api_key.path})"
-    '';
+    # programs.zsh.initContent = ''
+    #   export ANTHROPIC_API_KEY="$(cat ${config.sops.secrets.anthropic_api_key.path})"
+    # '';
   };
 }
