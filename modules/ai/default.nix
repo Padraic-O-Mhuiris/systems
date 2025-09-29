40a9d0f15caaf0ba5577ae8d642c0f0688344369
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2025-09-29 15:59:38 +0100

    symlink user claude dir

--- modules/ai/default.nix
+++ modules/ai/default.nix
@@ -6,8 +6,33 @@
   home-manager.users.${vars.PRIMARY_USER.NAME} = {
     config,
     pkgs,
+    lib,
     ...
-  }: {
+  }: let
+    claudeSrcPath = "/home/${vars.PRIMARY_USER.NAME}/systems/modules/ai/claude";
+  in {
+    home.file.".claude" = {
+      source = lib.mkForce (
+        config.lib.file.mkOutOfStoreSymlink claudeSrcPath
+      );
+      recursive = true;
+    };
+
+    home.file.".claude/settings.json".text =
+      #json
+      ''
+        {
+          "apiKeyHelper": "cat ${config.sops.secrets.anthropic_api_key.path}"
+        }
+      '';
+
+    # {
+    #   source = lib.mkForce (
+    #     config.lib.file.mkOutOfStoreSymlink claudeSrcPath
+    #   );
+    #   recursive = true;
+    # };
+
     home.packages = with inputs.nix-ai-tools.packages.${pkgs.system}; [claudebox claude-desktop claude-code];
 
     programs.zsh.initContent = ''
