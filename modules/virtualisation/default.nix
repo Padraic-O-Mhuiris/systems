ca197a47c36937757f47fa4c74e3f5605d556d5b
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2025-09-29 14:51:55 +0100

    virtualisation containers

--- modules/virtualisation/default.nix
+++ modules/virtualisation/default.nix
@@ -1,17 +1,31 @@
 {vars, ...}: {
-  virtualisation.docker = {
-    enable = false;
-    rootless = {
+  virtualisation = {
+    containers.enable = true;
+    podman = {
       enable = true;
-      setSocketVariable = true;
-      # Optionally customize rootless Docker daemon settings
-      daemon.settings = {
-        dns = ["1.1.1.1" "8.8.8.8"];
-        registry-mirrors = ["https://mirror.gcr.io"];
-        data-root = "/home/${vars.PRIMARY_USER.NAME}/.local/state/docker-data";
-      };
+      # autoPrune = true;
+      # Create a `docker` alias for podman, to use it as a drop-in replacement
+      dockerCompat = true;
+      # Required for containers under podman-compose to be able to talk to each other.
+      defaultNetwork.settings.dns_enabled = true;
     };
+
+    oci-containers.backend = "podman";
   };
 
-  # users.users.${vars.PRIMARY_USER.NAME}.extraGroups = ["docker"];
+  # virtualisation.docker = {
+  #   enable = false;
+  #   rootless = {
+  #     enable = true;
+  #     setSocketVariable = true;
+  #     # Optionally customize rootless Docker daemon settings
+  #     daemon.settings = {
+  #       dns = ["1.1.1.1" "8.8.8.8"];
+  #       registry-mirrors = ["https://mirror.gcr.io"];
+  #       data-root = "/home/${vars.PRIMARY_USER.NAME}/.local/state/docker-data";
+  #     };
+  #   };
+  # };
+
+  users.users.${vars.PRIMARY_USER.NAME}.extraGroups = ["podman"];
 }
