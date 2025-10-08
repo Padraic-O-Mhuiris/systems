b934755fbfe41a9be11c4dabdf5d6e1c2afdd2bc
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2025-10-08 13:53:04 +0100

    use docker

--- profiles/virtualisation/default.nix
+++ profiles/virtualisation/default.nix
@@ -1,16 +1,43 @@
 {vars, ...}: {
-  virtualisation = {
-    containers.enable = true;
-    podman = {
-      enable = true;
-      # autoPrune = true;
-      # Create a `docker` alias for podman, to use it as a drop-in replacement
-      dockerCompat = true;
-      # Required for containers under podman-compose to be able to talk to each other.
-      defaultNetwork.settings.dns_enabled = true;
-    };
+  # virtualisation = {
+  #   containers.enable = true;
+  #   podman = {
+  #     enable = true;
+  #     # autoPrune = true;
+  #     # Create a `docker` alias for podman, to use it as a drop-in replacement
+  #     dockerCompat = true;
+  #     # Required for containers under podman-compose to be able to talk to each other.
+  #     defaultNetwork.settings.dns_enabled = true;
+  #   };
 
-    oci-containers.backend = "podman";
+  #   oci-containers.backend = "podman";
+  # };
+  virtualisation.docker = {
+    enable = true;
+    # Customize Docker daemon settings using the daemon.settings option
+    daemon.settings = {
+      dns = ["1.1.1.1" "8.8.8.8"];
+      log-driver = "journald";
+      registry-mirrors = ["https://mirror.gcr.io"];
+      storage-driver = "overlay2";
+      # Do not have enough storage state in root
+      data-root = "/home/${vars.PRIMARY_USER.NAME}/.local/state/docker-data";
+    };
+    # Use the rootless mode - run Docker daemon as non-root user
+    rootless = {
+      enable = true;
+      setSocketVariable = true;
+    };
+  };
+
+  users.users.${vars.PRIMARY_USER.NAME}.extraGroups = ["docker"];
+
+  # K3s needs access
+  # boot.kernelParams = ["cgroup_enable=memory" "cgroup_enable=cpuset" "cgroup_memory=1"];
+
+  # Allows k3s to delegate user control over system resources
+  systemd.services."user@".serviceConfig = {
+    Delegate = "cpu cpuset io memory pids";
   };
 
   # virtualisation.docker = {
@@ -26,6 +53,4 @@
   #     };
   #   };
   # };
-
-  users.users.${vars.PRIMARY_USER.NAME}.extraGroups = ["podman"];
 }
