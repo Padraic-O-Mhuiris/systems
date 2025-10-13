908a4e996767e8026db05c0bfaff681088b965f0
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2025-10-13 14:25:24 +0100

    attempts at spotify keyring fixes

--- profiles/apps/spotify.nix
+++ profiles/apps/spotify.nix
@@ -1,6 +1,22 @@
 {vars, ...}: {
   home-manager.users.${vars.PRIMARY_USER.NAME} = {pkgs, ...}: {
-    home.packages = [pkgs.spotify];
+    home.packages = [
+      pkgs.spotify
+      # TODO The below is some failed workings on the annoying keyring popup. This is consequential of
+      # the source being installed from snapcraft and doing some internal jangling. There may be some
+      # env fixes which may help but needs investigation
+      #
+      # (pkgs.symlinkJoin {
+      #   name = "spotify";
+      #   paths = [pkgs.spotify];
+      #   buildInputs = [pkgs.makeWrapper];
+      #   postBuild = ''
+      #     wrapProgram $out/bin/spotify \
+      #       --set DBUS_FATAL_WARNINGS 0 \
+      #       --add-flags "--password-store=basic"
+      #   '';
+      # })
+    ];
   };
 
   networking.firewall = {
