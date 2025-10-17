ff1a2b21d4ab21c0628e3348f9c7216bf8aedd4d
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2025-10-17 12:17:23 +0100

    removed old agent text

--- nix/modules/homeManager/cc.nix
+++ nix/modules/homeManager/cc.nix
@@ -462,7 +462,7 @@ in {
         ];
       };
     in
-      pkgs.runCommand "claude" {
+      pkgs.runCommand "cc" {
         buildInputs = [pkgs.makeWrapper];
       } ''
         mkdir -p $out/bin
