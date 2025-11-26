fd768c92cd6fa47c7bbf3cdfe72e984193c11648
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2025-11-26 14:47:55 +0000

    making ghostty default terminal

--- profiles/terminal/wezterm/default.nix
+++ profiles/terminal/wezterm/default.nix
@@ -1,16 +1,11 @@
 {vars, ...}: {
-  home-manager.users.${vars.PRIMARY_USER.NAME} = {
-    config,
-    pkgs,
-    lib,
-    ...
-  }: {
+  home-manager.users.${vars.PRIMARY_USER.NAME} = {pkgs, ...}: {
     home.packages = with pkgs; [wezterm];
 
-    home.sessionVariables = {
-      TERMINAL = lib.getExe pkgs.wezterm;
-    };
+    # home.sessionVariables = {
+    #   TERMINAL = lib.getExe pkgs.wezterm;
+    # };
 
-    xdg.configFile."wezterm/wezterm.lua".source = config.lib.file.mkOutOfStoreSymlink "/home/${vars.PRIMARY_USER.NAME}/systems/profiles/terminal/wezterm/wezterm.lua";
+    # xdg.configFile."wezterm/wezterm.lua".source = config.lib.file.mkOutOfStoreSymlink "/home/${vars.PRIMARY_USER.NAME}/systems/profiles/terminal/wezterm/wezterm.lua";
   };
 }
