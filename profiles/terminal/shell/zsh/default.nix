c9e1695a4a608b3eb9d170aef461e557a181fc71
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2025-11-26 15:04:53 +0000

    format zsh

--- profiles/terminal/shell/zsh/default.nix
+++ profiles/terminal/shell/zsh/default.nix
@@ -29,42 +29,42 @@
       };
 
       zsh = {
-      enable = true;
-      dotDir = "${config.xdg.configHome}/zsh";
-      autosuggestion.enable = true;
-      syntaxHighlighting.enable = true;
-      enableCompletion = true;
-      enableVteIntegration = true;
-      autocd = true;
-      historySubstringSearch.enable = true;
-      history = {
-        expireDuplicatesFirst = true;
-        extended = true;
-        ignoreDups = true;
-        ignorePatterns = [];
-        ignoreSpace = true;
-        path = "${config.xdg.dataHome}/zsh/zsh_history";
-        save = 100000;
-        share = true;
-      };
-      oh-my-zsh = {
         enable = true;
-        plugins = [
-          "git"
-          "aliases"
-          "sudo"
-          "direnv"
-          "emoji"
-          "encode64"
-          "jsontools"
-          "systemd"
-          "dirhistory"
-          "command-not-found"
-          "colored-man-pages"
-          "extract"
-        ];
+        dotDir = "${config.xdg.configHome}/zsh";
+        autosuggestion.enable = true;
+        syntaxHighlighting.enable = true;
+        enableCompletion = true;
+        enableVteIntegration = true;
+        autocd = true;
+        historySubstringSearch.enable = true;
+        history = {
+          expireDuplicatesFirst = true;
+          extended = true;
+          ignoreDups = true;
+          ignorePatterns = [];
+          ignoreSpace = true;
+          path = "${config.xdg.dataHome}/zsh/zsh_history";
+          save = 100000;
+          share = true;
+        };
+        oh-my-zsh = {
+          enable = true;
+          plugins = [
+            "git"
+            "aliases"
+            "sudo"
+            "direnv"
+            "emoji"
+            "encode64"
+            "jsontools"
+            "systemd"
+            "dirhistory"
+            "command-not-found"
+            "colored-man-pages"
+            "extract"
+          ];
+        };
       };
-    };
     };
   };
 }
