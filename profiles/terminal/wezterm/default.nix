eddb8880a10a96acab68ab33a225c6c523b9b84e
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2025-11-26 17:08:51 +0000

    reenable wezterm

--- profiles/terminal/wezterm/default.nix
+++ profiles/terminal/wezterm/default.nix
@@ -1,11 +1,16 @@
 {vars, ...}: {
-  home-manager.users.${vars.PRIMARY_USER.NAME} = {pkgs, ...}: {
+  home-manager.users.${vars.PRIMARY_USER.NAME} = {
+    pkgs,
+    lib,
+    config,
+    ...
+  }: {
     home.packages = with pkgs; [wezterm];
 
-    # home.sessionVariables = {
-    #   TERMINAL = lib.getExe pkgs.wezterm;
-    # };
+    home.sessionVariables = {
+      TERMINAL = lib.getExe pkgs.wezterm;
+    };
 
-    # xdg.configFile."wezterm/wezterm.lua".source = config.lib.file.mkOutOfStoreSymlink "/home/${vars.PRIMARY_USER.NAME}/systems/profiles/terminal/wezterm/wezterm.lua";
+    xdg.configFile."wezterm/wezterm.lua".source = config.lib.file.mkOutOfStoreSymlink "/home/${vars.PRIMARY_USER.NAME}/systems/profiles/terminal/wezterm/wezterm.lua";
   };
 }
